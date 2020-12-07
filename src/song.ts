ffc163165e9f71cb4e127c1745c805b6d87da138
nawhi <25394965+nawhi@users.noreply.github.com>
2020-12-07 17:04:27 +0000

    Change to numeric ordinals instead of full words

--- src/song.ts
+++ src/song.ts
@@ -1,30 +1,30 @@
 const song = `
-On the first day of Christmas my true love sent to me
+On the 1st day of Christmas my true love sent to me
 And a partridge in a pear tree.
 
-On the second day of Christmas my true love sent to me
+On the 2nd day of Christmas my true love sent to me
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the third day of Christmas my true love sent to me
+On the 3rd day of Christmas my true love sent to me
 Three french hens,
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the fourth day of Christmas my true love sent to me
+On the 4th day of Christmas my true love sent to me
 Four calling birds,
 Three french hens,
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the fifth day of Christmas my true love sent to me
+On the 5th day of Christmas my true love sent to me
 Five golden rings,
 Four calling birds,
 Three french hens,
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the sixth day of Christmas, my true love sent to me
+On the 6th day of Christmas, my true love sent to me
 Six geese a-laying,
 Five golden rings,
 Four calling birds,
@@ -32,7 +32,7 @@ Three french hens,
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the seventh day of Christmas my true love sent to me
+On the 7th day of Christmas my true love sent to me
 Seven swans a-swimming,
 Six geese a-laying,
 Five golden rings,
@@ -41,7 +41,7 @@ Three french hens,
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the eighth day of Christmas my true love sent to me
+On the 8th day of Christmas my true love sent to me
 Eight maids a-milking,
 Seven swans a-swimming,
 Six geese a-laying,
@@ -51,7 +51,7 @@ Three french hens,
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the ninth day of Christmas, my true love sent to me
+On the 9th day of Christmas, my true love sent to me
 Nine ladies dancing,
 Eight maids a-milking,
 Seven swans a-swimming,
@@ -62,7 +62,7 @@ Three french hens,
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the tenth day of Christmas, my true love sent to me
+On the 10th day of Christmas, my true love sent to me
 Ten lords a-leaping,
 Nine ladies dancing,
 Eight maids a-milking,
@@ -74,7 +74,7 @@ Three french hens,
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the eleventh day of Christmas, my true love sent to me
+On the 11th day of Christmas, my true love sent to me
 Eleven pipers piping,
 Ten lords a-leaping,
 Nine ladies dancing,
@@ -87,7 +87,7 @@ Three french hens,
 Two turtle doves,
 And a partridge in a pear tree.
 
-On the twelfth day of Christmas, my true love sent to me
+On the 12th day of Christmas, my true love sent to me
 Twelve drummers drumming,
 Eleven pipers piping,
 Ten lords a-leaping,
