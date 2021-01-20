df2f0eca78569d03aa6f53521500dd70fb712474
nawhi <25394965+nawhi@users.noreply.github.com>
2021-01-20 17:11:14 +0000

    Remove array literals from generateSwallowedLines

--- src/song.ts
+++ src/song.ts
@@ -2,10 +2,13 @@ function generateSwallowedLine(predator: string, prey: string): string {
   return `She swallowed the ${predator} to catch the ${prey}`;
 }
 
-function generateSwallowedLines(animals: [string, string, ...string[]]): string {
+function generateSwallowedLines(animals: string[]): string {
+  if (animals.length < 2) {
+    throw new Error("needed an array of at least length 2");
+  }
   if (animals.length > 2) {
     const [prey, predator] = animals.slice(-2)
-    return `${generateSwallowedLine(predator, prey)},\n${generateSwallowedLines(animals.slice(0, -1) as any)}`
+    return `${generateSwallowedLine(predator, prey)},\n${generateSwallowedLines(animals.slice(0, -1))}`
   }
   return `${generateSwallowedLine(animals[1], animals[0])};`
 }
@@ -15,27 +18,27 @@ I don't know why she swallowed a ${animals[0]} - perhaps she'll die!
 
 There was an old lady who swallowed a ${animals[1]};
 That wriggled and wiggled and tickled inside her.
-${generateSwallowedLines([animals[0], animals[1]])}
+${generateSwallowedLines(animals.slice(0, 2))}
 I don't know why she swallowed a ${animals[0]} - perhaps she'll die!
 
 There was an old lady who swallowed a ${animals[2]};
 How absurd to swallow a ${animals[2]}.
-${generateSwallowedLines([animals[0], animals[1], animals[2]])}
+${generateSwallowedLines(animals.slice(0, 3))}
 I don't know why she swallowed a ${animals[0]} - perhaps she'll die!
 
 There was an old lady who swallowed a ${animals[3]};
 Fancy that to swallow a ${animals[3]}!
-${generateSwallowedLines([animals[0], animals[1], animals[2], animals[3]])}
+${generateSwallowedLines(animals.slice(0, 4))}
 I don't know why she swallowed a ${animals[0]} - perhaps she'll die!
 
 There was an old lady who swallowed a ${animals[4]};
 What a hog, to swallow a ${animals[4]}!
-${generateSwallowedLines([animals[0], animals[1], animals[2], animals[3], animals[4]])}
+${generateSwallowedLines(animals.slice(0, 5))}
 I don't know why she swallowed a ${animals[0]} - perhaps she'll die!
 
 There was an old lady who swallowed a ${animals[5]};
 I don't know how she swallowed a ${animals[5]}!
-${generateSwallowedLines([animals[0], animals[1], animals[2], animals[3], animals[4], animals[5]])}
+${generateSwallowedLines(animals.slice(0, 6))}
 I don't know why she swallowed a ${animals[0]} - perhaps she'll die!
 
 There was an old lady who swallowed a ${animals[6]}...
